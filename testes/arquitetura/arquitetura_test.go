89cc0f608c1baa35939c19b65105b0a7295bea9f
anahelenateodoro <anahelenafaustinoteodoro@gmail.com>
2022-09-28 22:42:46 -0300

    test arquitetura

---
+++ testes/arquitetura/arquitetura_test.go
@@ -0,0 +1,11 @@
+package arquitetura
+
+import "testing"
+
+func TestDependente(t *testing.T) {
+	if runtime.GOARCH == "amd64" {
+		t.Skip("Não funciona em arquitetura amd64")
+	}
+	//...
+	t.Fail()
+}
